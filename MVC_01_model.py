187154a38e325f1ed26fdae9226127c887483c08
Sapranov Andrey <andreika9198@gmail.com>
2026-06-11 09:03:01 +0300

    в файле MVC_01_model.py написал docstring

--- MVC_01_model.py
+++ MVC_01_model.py
@@ -1,5 +1,6 @@
 
 class PizzaModel:
+    """Класс, представляющий модель пиццы с её характеристиками и ингредиентами."""
 
     def __init__(self, name, price, weight):
         self.__name = name  # название товара/блюда
@@ -9,18 +10,22 @@ class PizzaModel:
 
     @property
     def name(self):
+        """Метод для получения название пиццы"""
         return self.__name
 
     @property
     def ingredients(self):
+        """Метод для получения ингредиентов"""
         return self.__ingredients.copy()
 
     @property
     def price(self):
+        """Метод для получения цены пиццы"""
         return self.__price
 
     @price.setter
     def price(self,new_price):
+        """Метод для изменения цены пиццы"""
         if new_price < 0:
             raise ValueError("Цена не может быть отрицательной")
         if new_price == self.__price:
@@ -30,23 +35,27 @@ class PizzaModel:
 
     @property
     def weight(self):
+        """Метод для получения веса пиццы"""
         return self.__weight
 
     @weight.setter
     def weight(self, new_weight):
+        """Метод для изменения веса пиццы"""
         if new_weight < 0:
             raise ValueError("Вес не может быть отрицательным")
         if new_weight == self.__weight:
-            raise ValueError('Новый вес совпадает с текущей')
+            raise ValueError('Новый вес совпадает с текущим')
         self.__weight = new_weight
 
 
-    def add_ingredients(self, ingredients):
-        self.__ingredients.append(ingredients)
+    def add_ingredient(self, ingredient):
+        """Метод для добавления ингредиента"""
+        self.__ingredients.append(ingredient)
 
 
-    def delete_ingredients(self,ingredient):
-        if len(self.__ingredients) <= 0:
+    def delete_ingredient(self,ingredient):
+        """Метод для удаления ингредиента"""
+        if not self.__ingredients:
             raise ValueError('Ингредиентов нет')
         try:
             self.__ingredients.remove(ingredient)
@@ -64,6 +73,7 @@ class PizzaModel:
             raise ValueError(f'Ингредиент "{old_ingredient}" не найден')
 
     def get_pizza(self):
+        """Метод возвращает данные о пицце"""
         data = {
             'name': self.__name,
             'ingredients': self.__ingredients.copy(),
